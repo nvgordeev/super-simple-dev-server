3cc251d0772471f97aea68bedf7c41e1e02889b9
nv-gordeev <nv-gordeev@ya.ru>
2019-03-17 17:58:18 +0300

    update

--- longPollingHandler.js
+++ longPollingHandler.js
@@ -6,8 +6,9 @@ module.exports = servingPath => (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*' });
 
   const timeout = setTimeout(() => {
+    watcher.close();
     response.end('heartbeat', 'utf-8');
-  }, 100000);
+  }, 1000);
 
   const handleChange = (evt, filename) => {
     console.log(`${filename} changed, reloading....`);
